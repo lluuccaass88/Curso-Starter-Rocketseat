9ae932fcacaf38aff15ef16075ab8387c4d045cf
lluuccaass88 <lucaspalves8@gmail.com>
2020-07-27 15:49:39 -0300

    commit

---
+++ 4-ModuloReactJS/huntewb/src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css'
+
+const Header = () => {
+    return(
+        <header id="main-header">JsHunt</header>
+    )
+}
+
+export default Header
--- README.md
+++
@@ -1,2 +0,0 @@
-# cursoStarterRocketseat
- Exercícios do curso starter da rocketseat, sobre JavaScript, Node, React JS e React Nativa
