6ce39781106038afb95895fafcad5bf4932286ab
lluuccaass88 <lucaspalves8@gmail.com>
2020-08-14 10:57:35 -0300

    commit

--- 5-ModuloReactNative/hunt/App.js
+++ 5-ModuloReactNative/hunt/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Index from './src/index'
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Index />
+      <Text> oi </Text>
+   
     </View>
   );
 }
--- 5-ModuloReactNative/hunt/src/routes.js
+++ 5-ModuloReactNative/hunt/src/routes.js
@@ -1,4 +1,4 @@
-import { createStackNavigator } from 'react-navigation-stack' //Pesquisar no site da rocketseat sobre react-navigation
+import { createStackNavigator } from 'react-navigation' //Pesquisar no site da rocketseat sobre react-navigation
 
 import Main from './pages/main'
 
